e6788cf593636d4bc2f1799c6bd665f780ed25fb
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-05-22 21:27:19 -0700

    add preliminary passing tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,44 @@
 //! Rust implementation of the manifold representations in manif-geom-rs
- 
+
+// use num_traits::Float; TODO remove?
+
+use rand::distributions::{Distribution, Standard};
+
 extern crate nalgebra as na;
+use na::{Scalar, Unit, Vector1, Vector2, Vector3, Vector4, Vector6, Matrix};
 
-use na::{Vector1, Vector2, Vector3, Vector4, Vector6, Matrix};
+/// Implemented manifold types : Float + na::ComplexField
 
-/// Implemented manifold types
-
-struct SO2<T> {
-    w: T,
-    x: T,
+struct SO2<T: Scalar + na::ComplexField> {
+    arr: Unit<Vector2<T>>, // w, x
 }
-struct SE2<T> {
+struct SE2<T: Scalar + na::ComplexField> {
     t: Vector2<T>,
     q: SO2<T>,
 }
-struct SO3<T> {
-    w: T,
-    v: Vector3<T>,
+struct SO3<T: Scalar + na::ComplexField> {
+    arr: Unit<Vector4<T>>, // w, x, y, z
 }
-struct SE3<T> {
+struct SE3<T: Scalar + na::ComplexField> {
     t: Vector3<T>,
     q: SO3<T>,
 }
 
 /// SO2 implementation
 
-impl<T> SO2<T> {
-    fn w(&self) -> &T {
-        &self.w
+impl<T: Scalar + na::ComplexField> SO2<T> {
+    pub fn random() -> SO2<T> 
+    where Standard: Distribution<T>, {
+        return SO2{ arr: Unit::new_normalize(Vector2::new_random()) };
     }
-    fn x(&self) -> &T {
-        &self.x
+    pub fn fromComplex(qw: T, qx: T) -> SO2<T> {
+        return SO2{ arr: Unit::new_normalize(Vector2::new(qw, qx)) };
+    }
+    pub fn w(&self) -> &T {
+        return &self.arr[(0, 0)]
+    }
+    pub fn x(&self) -> &T {
+        return &self.arr[(1, 0)]
     }
 }
 
@@ -41,11 +49,11 @@ mod test {
     use na::{Vector1, Vector2, Vector3, Vector4, Vector6, Matrix};
 
     static EPSILON: f64 = 0.000001;
-    static PI: f64 = 3.14159265358979323846264338327950288_f64;
+    // static PI: f64 = 3.14159265358979323846264338327950288_f64;
 
     #[test]
     fn test_so2_getters_tmp() {
-        let q: SO2<f64> = { SO2 { w: 1.0, x: 0.0 } };
+        let q: SO2<f64> = SO2::fromComplex(1.0, 0.0);
         assert!((q.w() - 1.0).abs() < EPSILON);
         assert!(q.x().abs() < EPSILON);
     }
