49a0738eab9f5cecaea610c30f0d9654dbc86ed4
Andrew Torgesen <atorgesen@anduril.com>
2024-01-19 23:24:53 -0800

    wip

--- src/so2.rs
+++ src/so2.rs
@@ -7,44 +7,47 @@ extern crate nalgebra as na;
 
 /// SO2 implementation
 
-pub struct SO2<T: na::Scalar + na::ComplexField> {
+pub struct SO2<T: na::Scalar + na::ComplexField + na::RealField> {
     arr: na::Unit<na::Vector2<T>>, // w, x
 }
 
-impl<T: na::Scalar + na::ComplexField> Default for SO2<T> {
+impl<T: na::Scalar + na::ComplexField + na::RealField> Default for SO2<T> {
     fn default() -> Self {
         Self { arr: na::Unit::new_unchecked(na::Vector2::new(na::convert(1.0), na::convert(0.0))) }
     }
 }
 
-impl<T: na::Scalar + na::ComplexField> SO2<T> {
-    pub fn new(q: na::Unit<na::Vector2<T>>) {
-        Self { q }
+impl<T: na::Scalar + na::ComplexField + na::RealField> SO2<T> {
+    pub fn new(q: na::Unit<na::Vector2<T>>) -> SO2<T> {
+        Self { arr: q }
     }
     pub fn random() -> SO2<T> 
     where rand::distributions::Standard: rand::distributions::Distribution<T>, {
-        return SO2{ arr: na::Unit::new_normalize(na::Vector2::new_random()) }
+        SO2{ arr: na::Unit::new_normalize(na::Vector2::new_random()) }
     }
     pub fn identity() -> SO2<T> {
-        return SO2{ arr: na::Unit::new_unchecked(na::Vector2::new(na::convert(1.0), na::convert(0.0))) }
+        SO2{ arr: na::Unit::new_unchecked(na::Vector2::new(na::convert(1.0), na::convert(0.0))) }
     }
-    pub fn fromAngle(angle: T) -> SO2<T> {
+    pub fn fromAngle(angle: &T) -> SO2<T> {
         let c: T = angle.clone().cos();
         let s: T = angle.clone().sin();
-        return SO2 { arr: na::Unit::new_unchecked(na::Vector2::new(na::convert(c), na::convert(s))) }
+        SO2 { arr: na::Unit::new_unchecked(na::Vector2::new(na::convert(c), na::convert(s))) }
     }
-    pub fn fromR(m: na::Matrix2<T>) -> SO2<T> {
-        return SO2 { arr: na::Unit::new_normalize(na::Vector2::new(m[(0, 0)].clone(), m[(1, 0)].clone())) }
+    pub fn fromR(m: &na::Matrix2<T>) -> SO2<T> {
+        SO2 { arr: na::Unit::new_normalize(na::Vector2::new(m[(0, 0)].clone(), m[(1, 0)].clone())) }
     }
     // fromTwoUnitVectors
     pub fn fromComplex(qw: T, qx: T) -> SO2<T> {
-        return SO2{ arr: na::Unit::new_normalize(na::Vector2::new(qw, qx)) };
+        SO2{ arr: na::Unit::new_normalize(na::Vector2::new(qw, qx)) }
     }
     pub fn w(&self) -> &T {
-        return &self.arr[(0, 0)]
+        &self.arr[(0, 0)]
     }
     pub fn x(&self) -> &T {
-        return &self.arr[(1, 0)]
+        &self.arr[(1, 0)]
+    }
+    pub fn angle(&self) -> T {
+        self.w().clone().atan2(self.x().clone())
     }
     // array
     // copy
@@ -75,7 +78,14 @@ mod test {
             let mut v3: Vector3<f64> = Vector3::identity();
             v3[0] = v2[0];
             v3[1] = v2[1];
-            let q_so3: SO3<f64> = SO3::fromEuler(0.0, 0.0, q_so2.angle());
+            let q_so3: SO3<f64> = SO3::fromEuler(&0.0, &0.0, &q_so2.angle());
+
+            println!("{}", q_so3.roll());
+            assert!(q_so3.roll().abs() < EPSILON);
+            assert!(q_so3.pitch().abs() < EPSILON);
+            assert!((q_so3.yaw() - q_so2.angle()).abs() < EPSILON);
+
+            // let qv2: Vector2<f64> = q_so2 * v2; ^^^^ TODO
             // ++++
             i = i + 1;
         }
--- src/so3.rs
+++ src/so3.rs
@@ -3,77 +3,101 @@ use std::ops::Mul;
 
 /// SO3 implementation
 
-pub struct SO3<T: na::Scalar + na::ComplexField> {
+pub struct SO3<T: na::Scalar + na::ComplexField + na::RealField> {
     arr: na::Unit<na::Vector4<T>>, // w, x, y, z
 }
 
-impl<T: na::Scalar + na::ComplexField> Default for SO3<T> {
+impl<T: na::Scalar + na::ComplexField + na::RealField> Default for SO3<T> {
     fn default() -> Self {
         Self { arr: na::Unit::new_unchecked(na::Vector4::new(na::convert(1.0), na::convert(0.0), na::convert(0.0), na::convert(0.0))) }
     }
 }
 
-impl<T: na::Scalar + na::ComplexField> SO3<T> {
+impl<'a, 'b: 'a, T: na::Scalar + na::ComplexField + na::RealField> SO3<T> where &'b T: Mul<&'a T, Output = T> {
     pub fn new(q: na::Unit<na::Vector4<T>>) -> SO3<T> {
         Self { arr: q }
     }
     pub fn w(&self) -> &T {
-        return &self.arr[(0, 0)]
+        &self.arr[(0, 0)]
     }
     pub fn x(&self) -> &T {
-        return &self.arr[(1, 0)]
+        &self.arr[(1, 0)]
     }
     pub fn y(&self) -> &T {
-        return &self.arr[(2, 0)]
+        &self.arr[(2, 0)]
     }
     pub fn z(&self) -> &T {
-        return &self.arr[(3, 0)]
+        &self.arr[(3, 0)]
     }
-    pub fn fromAxisAngle(axis: na::Vector3<T>, angle: T) -> SO3<T> {
-        let th2: T = angle / T::two();
-        let xyz: na::Vector3<T> = (th2.clone().sin() / (axis.norm() as T)) * axis;
+    pub fn fromAxisAngle(axis: &na::Vector3<T>, angle: &T) -> SO3<T> {
+        let angle_scale: T = T::one() / na::convert(2.0);
+        let th2: T = angle_scale * angle.clone();
+        let axis_normalized: na::Vector3<T> = axis.normalize();
+        let scale: T = th2.clone().sin();
         let q_arr: na::Unit<na::Vector4<T>> = na::Unit::new_normalize(na::Vector4::new(
             th2.clone().cos(),
-            xyz[0], xyz[1], xyz[2]
+            scale.clone() * axis_normalized[0].clone(),
+            scale.clone() * axis_normalized[1].clone(),
+            scale.clone() * axis_normalized[2].clone()
         ));
-        return SO3{ arr: q_arr }
+        SO3{ arr: q_arr }
     }
-    pub fn fromEuler(roll: T, pitch: T, yaw: T) -> SO3<T> {
-        let q_roll: SO3<T> = SO3::fromAxisAngle(na::Vector3::new(1.0, 0.0, 0.0), roll);
-        let q_pitch: SO3<T> = SO3::fromAxisAngle(na::Vector3::new(0.0, 1.0, 0.0), pitch);
-        let q_yaw: SO3<T> = SO3::fromAxisAngle(na::Vector3::new(0.0, 0.0, 1.0), yaw);
+    pub fn fromEuler(roll: &T, pitch: &T, yaw: &T) -> SO3<T> {
+        let q_roll: SO3<T> = SO3::fromAxisAngle(&na::Vector3::x(), roll);
+        let q_pitch: SO3<T> = SO3::fromAxisAngle(&na::Vector3::y(), pitch);
+        let q_yaw: SO3<T> = SO3::fromAxisAngle(&na::Vector3::z(), yaw);
         let q_euler: SO3<T> = q_yaw * q_pitch * q_roll;
-        return q_euler
+        q_euler
     }
     pub fn roll(&self) -> T {
-        (T::one() - T::two() * (self.x() * self.x() + self.y() * self.y())).atan2(T::two() * (self.w() * self.x() + self.y() * self.z()))
+        let x: T = self.x().clone();
+        let y: T = self.y().clone();
+        let z: T = self.z().clone();
+        let w: T = self.w().clone();
+        (T::one() - (x.clone() * x.clone() + y.clone() * y.clone()) * na::convert(2.0)).atan2((w.clone() * x.clone() + y.clone() * z.clone()) * na::convert(2.0))
     }
     pub fn pitch(&self) -> T {
-        let val: T = T::two() * (self.w() * self.y() - self.x() * self.z());
+        let x: T = self.x().clone();
+        let y: T = self.y().clone();
+        let z: T = self.z().clone();
+        let w: T = self.w().clone();
+        let val: T =  (w.clone() * y.clone() - x.clone() * z.clone()) * na::convert(2.0);
         // hold at 90 degrees if invalid
-        if val.abs() > T::one() {
-            return (1.0_f64).copysign(val) * f64::PI / T::two();
+        if val.clone().abs() > na::convert(1.0) {
+            return T::one().copysign(val) * na::convert(std::f64::consts::PI / 2.0);
         }
         else {
             return val.asin();
         }
     }
     pub fn yaw(&self) -> T {
-        (1.0 - 2.0 * (self.y() * self.y() + self.z() * self.z())).atan2(2.0 * (self.w() * self.z() + self.x() * self.y()))
+        let x: T = self.x().clone();
+        let y: T = self.y().clone();
+        let z: T = self.z().clone();
+        let w: T = self.w().clone();
+        (T::one() - (y.clone() * y.clone() + z.clone() * z.clone()) * na::convert(2.0)).atan2((w.clone() * z.clone() + x.clone() * y.clone()) * na::convert(2.0))
     }
     pub fn otimes(&self, q: SO3<T>) -> SO3<T> {
-        return SO3{ arr: na::Unit::new_normalize(na::Vector4::new(
-            self.w() * q.w() - self.x() * q.x() - self.y() * q.y() - self.z() * q.z(),
-            self.w() * q.x() + self.x() * q.w() + self.y() * q.z() - self.z() * q.y(),
-            self.w() * q.y() - self.x() * q.z() + self.y() * q.w() + self.z() * q.x(),
-            self.w() * q.z() + self.x() * q.y() - self.y() * q.x() + self.z() * q.w()
+        let self_x: T = self.x().clone();
+        let self_y: T = self.y().clone();
+        let self_z: T = self.z().clone();
+        let self_w: T = self.w().clone();
+        let q_x: T = q.x().clone();
+        let q_y: T = q.y().clone();
+        let q_z: T = q.z().clone();
+        let q_w: T = q.w().clone();
+        SO3{ arr: na::Unit::new_normalize(na::Vector4::new(
+            self_w.clone() * q_w.clone() - self_x.clone() * q_x.clone() - self_y.clone() * q_y.clone() - self_z.clone() * q_z.clone(),
+            self_w.clone() * q_x.clone() + self_x.clone() * q_w.clone() + self_y.clone() * q_z.clone() - self_z.clone() * q_y.clone(),
+            self_w.clone() * q_y.clone() - self_x.clone() * q_z.clone() + self_y.clone() * q_w.clone() + self_z.clone() * q_x.clone(),
+            self_w.clone() * q_z.clone() + self_x.clone() * q_y.clone() - self_y.clone() * q_x.clone() + self_z.clone() * q_w.clone()
         )) }
     }
 }
 
-impl<T: na::Scalar + na::ComplexField> Mul for SO3<T> {
+impl<'a, 'b: 'a, T: na::Scalar + na::ComplexField + na::RealField> Mul for SO3<T> where &'b T: Mul<&'a T, Output = T> {
     type Output = Self;
     fn mul(self, rhs: Self) -> Self {
-        self.otimes(rhs);
+        self.otimes(rhs)
     }
 }
